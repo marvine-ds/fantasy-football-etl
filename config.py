1f839bb6d4031fd6497dcba21f3cd631b546cbe0
Marvine Ekina <ekina.marvine23@gmail.com>
2026-02-07 12:34:44 +0100

    Add API configuration

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 # config.py
 # Central configuration for the fantasy football data pipeline
+
 API_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 
-
+# Database configuration
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Marvine23#",     
+    "database": "fantasy_football"
+}
